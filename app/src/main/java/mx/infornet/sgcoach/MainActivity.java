48ab3757404477ca791b34bc206f6916ab750957
JLuis <luisbp93@gmail.com>
2019-11-28 12:32:05 -0600

    se agrego el Bottom Navigation

--- app/src/main/java/mx/infornet/sgcoach/MainActivity.java
+++ app/src/main/java/mx/infornet/sgcoach/MainActivity.java
@@ -1,14 +1,57 @@
 package mx.infornet.sgcoach;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_perfil:
+                            selectedFragment = new PerfilFragment();
+                            break;
+                        case R.id.nav_members:
+                            selectedFragment = new MembersFragment();
+                            break;
+                        case R.id.nav_routines:
+                            selectedFragment = new RoutinesFragment();
+                            break;
+                        case R.id.nav_food:
+                            selectedFragment = new FoodFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
