59258ec3724385a8330dae9e1dcc065f0c46ed89
JLuis <luisbp93@gmail.com>
2019-12-04 16:18:56 -0600

    About

--- app/src/main/java/mx/infornet/sgcoach/MainActivity.java
+++ app/src/main/java/mx/infornet/sgcoach/MainActivity.java
@@ -3,19 +3,17 @@ package mx.infornet.sgcoach;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,8 +21,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RequestQueue queue;
-    private StringRequest request_logout;
+
+    TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +38,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.nav_exit:/*
+                    case R.id.nav_exit:
+                        /*
                         request_logout = new StringRequest(Request.Method.POST, Config.LOGOUT_URL, new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
@@ -63,6 +62,13 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.nav_info:
                         Toast.makeText(MainActivity.this, "Presionaste el boton acerca", Toast.LENGTH_SHORT).show();
+                        //Despues de actualizar los datos volvemos a cargar el fragmen del perfil
+                        Fragment Home = new HomeFragment();
+                        //FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment_container, Home);
+                        transaction.addToBackStack(null);
+                        transaction.commit();
                         break;
                     case R.id.nav_problems:
                         Toast.makeText(MainActivity.this, "Presionaste el boton reportar un problema", Toast.LENGTH_SHORT).show();
